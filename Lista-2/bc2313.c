f13f657474987c2f2e61d9ffe13bbe40bfd672d7
Alysson <alysson3789f@gmail.com>
2026-09-07 16:13:49 -0300

    Adicionado comentarios ao bc2313

--- Lista-2/bc2313.c
+++ Lista-2/bc2313.c
@@ -2,28 +2,31 @@
 
 int main(void){
 
-    int a, b, c, maior;
+    int a, b, c, maior;                           // define as variaveis
 
     scanf("%d %d %d", &a, &b, &c);
 
-    if(a + b <= c || a + c <= b || b + c <= a){
+    if(a + b <= c || a + c <= b || b + c <= a){  // A soma de dois lados deve ser maior que o terceiro para formar um triângulo.
         printf("Invalido\n");
     }
     else{
-        if(a == b && b == c){
+        if(a == b && b == c){                    // Equilátero = todos os lados iguais
             printf("Valido-Equilatero\n");
         }
-        else if(a == b || a == c || b == c){
+        else if(a == b || a == c || b == c){     // Isosceles = dois lados iguais
             printf("Valido-Isoceles\n");
         }
         else{
-            printf("Valido-Escaleno\n");
+            printf("Valido-Escaleno\n");         // Todos os lados diferentes
         }
 
-        // descobrir o maior lado
-        maior = a;
+        maior = a;                               // descobrir o maior lado (maior lado = hipotenusa)
         if(b > maior) maior = b;
         if(c > maior) maior = c;
+
+
+
+        // Realizada pitagoras para saber se é retangulo ou nao
 
         if(maior == a && a*a == b*b + c*c){
             printf("Retangulo: S\n");
