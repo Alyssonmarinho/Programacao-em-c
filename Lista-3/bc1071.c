e8478612a7adee9871ba3b4757b12156ce9c690d
Alysson <alysson3789f@gmail.com>
2026-09-13 20:04:57 -0300

    Adicionado comentários ao bc1071

--- Lista-3/bc1071.c
+++ Lista-3/bc1071.c
@@ -10,9 +10,9 @@ int main(void){
 
     scanf("%d\n%d", &x, &y);
 
-    if(x > y){
-        aux = x;
-        x = y;
+    if(x > y){            // Troca x e y se necessário, pois o for precisa que x seja o menor
+        aux = x;          // e y o maior: se x > y, o laço (i = x+1; i < y; i++) nunca executaria,
+        x = y;            // já que i começaria maior que o limite de parada.
         y = aux;
     }
 
